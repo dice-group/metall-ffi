0504f14e0cc2145a02c07191ca1248917b22f65b
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2023-09-20 08:34:29 +0200

    Prevent early destruction of metall manager by interface change (#3)

--- src/dice/ffi/metall.h
+++ src/dice/ffi/metall.h
@@ -8,84 +8,78 @@
 extern "C" {
 #endif
 
+typedef struct metall_manager metall_manager;
+
 /**
  * @brief Attempts to open the metall datastore at path
+ * @param path path to datastore
  * @return true on success, false on failure. On failure, sets errno to one of the following values:
  *      - ENOTRECOVERABLE if the given metall datastore is inconsistent
  */
-bool metall_open(char const *path);
+metall_manager *metall_open(char const *path);
 
 /**
  * @brief Attempts to create a metall datastore at path
+ * @param path path at which to create a datastore
  * @return true on success, false on failure. On failure, sets errno to one of the following values:
  *      - EEXIST if the given path already exists
+ *      - ENOTRECOVERABLE if the datastore could not be created for some other reason
  */
-bool metall_create(char const *path);
+metall_manager *metall_create(char const *path);
 
 /**
- * @brief Checks if a metall manager is open for the given path
- * @return true if open otherwise false
+ * @brief Creates a snapshot of the metall datastore of manager and places it at dst_path
+ * @param manager manager to perform snapshot
+ * @param dst_path path where to place the snapshot
+ * @return true if the snapshot was successfully created otherwise false.
  */
-bool metall_is_open(char const *path);
+bool metall_snapshot(metall_manager *manager, char const *dst_path);
 
 /**
- * @brief checks if the datastore at the given path is in consistent state.
- * @return true if the datastore is consistent, false otherwise.
+ * @brief Closes a metall manager
  */
-bool metall_is_consistent(char const *path);
-
-/**
- * @brief Creates a snapshot of the metall datastore at src_path and places it at dst_path
- * @return true if the snapshot was successfully created otherwise false. On failure sets errno to one of the following values:
- *      - EINVAL if there is no metall datastore open at src_path
- */
-bool metall_snapshot(char const *src_path, char const *dst_path);
-
-/**
- * @brief Attempts to close the metall datastore opened at path
- * @return true on success, false on failure. On failure, sets errno to one of the following values:
- *      - EINVAL if the given path does not have a metall datastore open
- */
-bool metall_close(char const *path);
+void metall_close(metall_manager *manager);
 
 /**
  * @brief Removes the metall datastore at path
+ * @param path path to datastore to remove
  * @return true on successful removal, false otherwise. On failure, sets errno to one of the following values:
  *      - EADDRINUSE if there is a metall manager open for the given path
+ *
+ * @warning Behaviour is undefined if there is still a metall manager for path open
  */
 bool metall_remove(char const *path);
 
-
 /**
  * @brief Allocates size bytes and associates the allocated memory with a name
- * @param path path of the open datastore
+ * @param manager metall manager to allocate with
  * @param name A name of the allocated memory
  * @param size number of bytes to allocate
  * @return pointer to the allocated memory if sucessful otherwise returns NULL and sets errno to one of the following values
  *      - EINVAL if the given path does not have a metall datastore open
  *      - ENOMEM if the memory could not be allocated
  */
-void *metall_malloc(char const *path, char const *name, size_t size);
+void *metall_malloc(metall_manager *manager, char const *name, size_t size);
 
 /**
  * @brief Finds memory that was previously allocated using metall_named_alloc
- * @param path path of the open datastore
+ * @param manager manager to find the object in
  * @param name name of the allocated memory to find
  * @return pointer to the allocated memory if found. Otherwise, returns NULL and sets errno to one of the following values
  *      - EINVAL if the given path does not have a metall datastore open
  *      - ENOTENT if the object could not be found
  */
-void *metall_find(char const *path, char const *name);
+void *metall_find(metall_manager *manager, char const *name);
 
 /**
  * @brief Frees memory previously allocated by metall_named_malloc
- * @param path path of the open datastore
+ * @param manager manager from which to free
  * @param name name of the allocated memory to free
  * @return true if sucessfully freed, otherwise returns false and sets errno to one of the following values
  *      - EINVAL if the given path does not have a metall datastore open
  *      - ENOENT if the referred to object does not exist
  */
-bool metall_free(char const *path, char const *name);
+bool metall_free(metall_manager *manager, char const *name);
 
 #ifdef __cplusplus
 }
--- src/dice/ffi/metall_internal.hpp
+++ src/dice/ffi/metall_internal.hpp
@@ -1,14 +1,14 @@
 #ifndef DICE_METALLFFI_METALLINTERNAL_HPP
 #define DICE_METALLFFI_METALLINTERNAL_HPP
 
-#include <dice/sparse-map/sparse_map.hpp>
 #include <metall/metall.hpp>
 
 namespace dice::metall_ffi::internal {
-
+    /**
+     * @brief The metall manager type used internally.
+     *      This object type is whats actually behind the opaque ::metall_manager * in the interface
+     */
     using metall_manager = metall::basic_manager<uint32_t, (1ULL << 28ULL)>;
-    extern dice::sparse_map::sparse_map<std::string, std::unique_ptr<metall_manager>> metall_managers;
-
 } // namespace
 
 #endif//DICE_METALLFFI_METALLINTERNAL_HPP
