5e9f59b686b5093b3d8233aa2928d5efabf62ebc
Liss Heidrich <31625940+liss-h@users.noreply.github.com>
2024-09-17 11:58:06 +0200

    fix

--- src/dice/ffi/metall_internal.hpp
+++ src/dice/ffi/metall_internal.hpp
@@ -18,51 +18,13 @@ namespace dice::metall_ffi::internal {
       *      This object type is whats actually behind the opaque ::metall_manager * in the interface.
       *      This is a wrapper that contains the read_only flag for older versions that do not support read_only() yet.
       */
-     struct metall_manager {
-          metall::manager manager_;
+     struct metall_manager : metall::manager {
           bool read_only_;
 
           template<typename T>
           using allocator_type = typename metall::manager::template allocator_type<T>;
 
-          metall_manager(metall::create_only_t op, char const *path) : manager_{op, path},
-                                                                       read_only_{false} {
-          }
-
-          metall_manager(metall::open_only_t op, char const *path) : manager_{op, path},
-                                                                     read_only_{false} {
-          }
-
-          metall_manager(metall::open_read_only_t op, char const *path) : manager_{op, path},
-                                                                          read_only_{true} {
-          }
-
-          bool check_sanity() noexcept {
-               return manager_.check_sanity();
-          }
-
-          bool read_only() const noexcept {
-               return read_only_;
-          }
-
-          bool snapshot(char const *dst) {
-               return manager_.snapshot(dst);
-          }
-
-          template<typename T>
-          auto construct(char const *name) {
-               return manager_.construct<T>(name);
-          }
-
-          template<typename T>
-          auto find(char const *name) {
-               return manager_.find<T>(name);
-          }
-
-          template<typename T>
-          auto destroy(char const *name) {
-               return manager_.destroy<T>(name);
-          }
+          using metall::manager::manager;
      };
 #endif
 } // namespace
