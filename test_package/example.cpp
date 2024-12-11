8147e964029110b9e00e5e52a4b8cfb037daa01f
Liss Heidrich <31625940+liss-h@users.noreply.github.com>
2024-12-11 09:53:41 +0100

    fix

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,4 +1,5 @@
 #include <dice/ffi/metall.h>
+#include <cstring>
 
 #if __has_include(<metall/logger_interface.h>)
 extern "C" void metall_log(metall_log_level, char const *, size_t, char const *) {
