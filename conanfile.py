949f9a5f58a0ad015bf4a77f218c3a042c96ad20
Adrian Macal <adrian@tentris.io>
2026-07-08 11:18:20 +0000

    Pin boost to 1.90.0 to fix metall/0.32 build vs boost 1.91 API break

--- conanfile.py
+++ conanfile.py
@@ -30,6 +30,9 @@ class Recipe(ConanFile):
 
     def requirements(self):
         self.requires("metall/0.32", transitive_headers=True)
+        # metall/0.32 calls boost::interprocess's construct_n() with its pre-1.91 2-arg
+        # signature; boost 1.91 added a SegmentManager* parameter, breaking the build.
+        self.requires("boost/1.90.0", override=True)
 
         if self.options.with_test_deps:
             self.requires("doctest/2.4.11")
